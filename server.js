05c28edd1263eb51e5459d7d33231b07f54dcafb
Alla Tikhomirova <tikhomirova.aa95@gmail.com>
2022-10-28 18:15:36 +0300

    remove unused code

--- server.js
+++ server.js
@@ -7,24 +7,6 @@ require('dotenv').config();
 
 app.use(cors());
 
-// app.get('/flights', (req, res) => {
-//     const options = {
-//         method: 'GET',
-//         url: 'https://toronto-pearson-airport.p.rapidapi.com/departures',
-//         headers: {
-//           'X-RapidAPI-Key': process.env.RAPID_API_KEY,
-//           'X-RapidAPI-Host': 'toronto-pearson-airport.p.rapidapi.com'
-//         }
-//       };
-
-//       axios.request(options).then(function (response) {
-//           console.log(response.data);
-//           res.json(response.data.slice(0,6));
-//       }).catch(function (error) {
-//           console.error(error);
-//       });
-// })
-
 app.get('/flights', (req, res) => {
     const options = {
         method: 'GET',
